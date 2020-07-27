ce9e972d20b127191dfc37d93e689a8d8424ae78
Ross Rosenlof <rrosenlo@byu.edu>
2020-07-27 12:49:54 -0600

    hmmm

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,30 +1,53 @@
 import React from 'react';
 import Sketch from "react-p5";
 
-const P5Sketch = (props) => {
-    let cols = 40, rows = 40, s = 700, step = (s / cols), factor = 0.03;
-    let strings = ['🍒', '🍊', '🍕', '🍤', '🌽']
+export default class P5Sketch extends React.Component {
+  state = {
+    cols: 40,
+    rows: 40,
+    s: 700,
+    factor: 0.03,
+    strings: this.props.strings
+  }
 
-    const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(s, s - step).parent(canvasParentRef);
-        p5.textSize(step);
-    };
 
-    const draw = (p5) => {
-        p5.background(0);
+  setup = (p5, canvasParentRef) => {
+    var step = this.state.s / this.state.cols
+    p5.createCanvas(this.state.s, this.state.s - step).parent(canvasParentRef);
+    p5.textSize(step);
+    p5.fill(240)
+  };
 
-        for (let y = 1; y < rows; y++) {
-            for (let x = 0; x < cols; x++) {
-                var n = p5.noise(x * factor, y * factor, p5.frameCount * 0.01) * 2;
-                n = (n - p5.int(n)) * 3;
-                var cx = p5.sin(n);
-                var cy = p5.cos(n);
-                var i = p5.floor(p5.map(cx * cy, -0.5, 0.45, 0, strings.length - 1));
-                p5.text(strings[i], x * step + 3, y * step);
-            }
-        }
-    };
-    return <Sketch setup={setup} draw={draw} />;
-};
+  draw = (p5) => {
+    p5.background(0);
+    var step = this.state.s / this.state.cols
 
-export default P5Sketch
+    for (let y = 1; y < this.state.rows; y++) {
+      for (let x = 0; x < this.state.cols; x++) {
+        var n = p5.noise(x * this.state.factor, y * this.state.factor, p5.frameCount * 0.01) * 2;
+        n = (n - p5.int(n)) * 3;
+        var cx = p5.sin(n);
+        var cy = p5.cos(n);
+        var i = p5.floor(p5.map(cx * cy, -0.5, 0.45, 0, this.state.strings.length - 1));
+        p5.text(this.state.strings[i], x * step + 3, y * step);
+
+        console.log(this.state.strings[i])
+      }
+    }
+
+    p5.noLoop();
+  };
+
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  render() {
+    return <Sketch setup={this.setup} draw={this.draw} />
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,38 @@ import React from "react"
 import P5Sketch from "../components/sketch"
 import '../../static/styles.css'
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Sketches</h1>
-      <p>Made with <a href='https://p5js.org/' target="_">p5.js</a> by <a href='https://github.com/rrosenlof'>@rrosenlof</a></p>
-      <P5Sketch></P5Sketch>
-    </div>
-  )
+export default class Home extends React.Component {
+  state = {
+    strings: ''
+  }
+
+  handleInputChange = event => {
+    let s = [...this.state.strings]
+    s.push(event.target.value)
+    this.setState({
+      strings: s,
+    })
+  }
+
+  render() {
+    let sketch = '';
+    if (this.state.strings){
+      sketch = <P5Sketch strings={this.state.strings}></P5Sketch>;
+    } else {
+      sketch = ''
+    }
+    return (
+      <div>
+        <h1>Sketches</h1>
+        <p>Made with <a href='https://p5js.org/' target="_">p5.js</a> by <a href='https://github.com/rrosenlof'>@rrosenlof</a></p>
+        <form>
+          <input type="text" onChange={this.handleInputChange}></input>
+        </form>
+        {/* <P5Sketch strings={this.state.strings}></P5Sketch> */}
+        {sketch}
+        
+        
+      </div>
+    )
+  }
 }
