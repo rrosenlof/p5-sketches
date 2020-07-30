5507573e7276728806a69f1635fe706c05d1ee08
ross <rosenlof.ross@gmail.com>
2020-07-30 17:41:01 -0600

    using layout with index list of sketches

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default function Layout({ children }) {
   return (
-    <div style={{ maxWidth: `700px`,  margin: `0 auto`, padding: `0 1.2rem` }}>
+    <div style={{ maxWidth: `700px`, margin: `0 auto`, padding: `0 1.2rem` }}>
+      <h1>Sketches</h1>
+      <p>Made with <a href='https://p5js.org/' target="_">p5.js</a> by <a href='https://github.com/rrosenlof'>@rrosenlof</a></p>
       {children}
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,14 @@
 import React from "react"
-import P5Sketch from "../components/sketch"
 import '../../static/styles.css'
-import { EMOJIS } from '../../static/constants.js'
 import Layout from "../components/layout"
+import { Link } from 'gatsby'
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      strings: ['🥩','🍑','🧀','🥦','🧊','🍇'],
-    }
-
-    this.addEmoji = this.addEmoji.bind(this);
-  }
-
-  addEmoji = event => {
-    let s = [...this.state.strings];
-    let i = s.indexOf(event.target.value);
-    i === -1 ? s.push(event.target.value) : s.splice(i,1)
-    this.setState({
-      strings: s
-    });
-  }
-
+  
   render() {
     return (
       <Layout>
-        <h1>Sketches</h1>
-        <p>Made with <a href='https://p5js.org/' target="_">p5.js</a> by <a href='https://github.com/rrosenlof'>@rrosenlof</a></p>
-        <P5Sketch strings={this.state.strings}></P5Sketch>
-        <div className='buttons'>
-          {EMOJIS.map((value, index) => {
-            return <button key={index} onClick={this.addEmoji} value={value}>{value}</button>
-          })}
-        </div>
-        <br />
-        <p style={{innerHeight: `1rem`}}>{this.state.strings}</p>
+        <Link to='/emoji-grid'>Emoji Grid</Link>
       </Layout>
     )
   }
