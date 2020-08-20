688a2ac0eb89ff1c4746cb9f26818640f0c807c5
Ross Rosenlof <rrosenlo@byu.edu>
2020-08-20 14:35:57 -0600

    couple layout changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ export default function Layout({ children }) {
   return (
     <div style={{ maxWidth: `700px`, margin: `0 auto`, padding: `0 1.2rem` }}>
       <Link to='/'><h1>P5 Sketches</h1></Link>
+      <hr />
       {children}
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,12 @@ export default class Home extends React.Component {
     return (
       <Layout>
         <p>Made with <a href='https://p5js.org/' target="_">p5.js</a> by <a href='https://github.com/rrosenlof'>@rrosenlof</a></p>
+        <div style={{ marginLeft: `1rem`}}>
         <ul>
-          <li><Link to='/emoji-grid'>Emoji Grid</Link></li>
+          <li><Link to='/emoji-grid'>Emoji Grid</Link>: A Perlin noise field using various Emojis as characters</li>
         </ul>
-        
+        </div>
+        <p style={{ fontStyle: `italic`, fontSize: `90%`}}><b>Warning:</b> Some of these sketches have flashing images or designs that may affect those prone to seizures or epilepsy</p>
       </Layout>
     )
   }
